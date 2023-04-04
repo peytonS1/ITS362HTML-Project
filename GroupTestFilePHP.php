51403ddcbb4e19c79a8750a7fce764e4b062ebaf
peytonS1 <94859266+peytonS1@users.noreply.github.com>
2023-04-04 14:45:15 -0500

    Add files via upload

---
+++ GroupTestFilePHP.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "Name is: " . $_GET['name'] . "<br>";
+            echo "Email is: " . $_GET['email'] . "<br>";
+            echo "Age is: " . $_GET['age'] . "<br>";
+            echo "City is: " . $_GET['city'] . "<br>";
+        ?>
+    </body>
+</html>
