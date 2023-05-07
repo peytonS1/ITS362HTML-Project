788aad6cbf852d5396949e9acb8bbea0af8bc8dc
laramj2414 <129987781+laramj2414@users.noreply.github.com>
2023-05-07 13:13:41 -0500

    Update Home.php

--- Home.php
+++ Home.php
@@ -25,13 +25,31 @@ try{
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <style>
-    .video {
+     .video {
         position: absolute;
-        left: 49%;
+        left: 46.25%;
         top: 85%;
         margin-left: -230px; 
-        margin-top: -100px; 
+        margin-top: -50px; 
   }
+
+    #frm{
+        font: bold;
+    }
+
+    .centerTop{
+        font-size: 50px;
+        left: 41%;
+    }
+
+    .leftCenter{
+        margin-top: 60px;
+        left: 36%;
+    }
+
+    .imageLogo{
+        margin-top: -30px;
+    }
     </style>
 </head>
 <body style = background-color:rgb(255,196,206)>
@@ -46,6 +64,45 @@ try{
     <img class="imageLogo" src="https://4.bp.blogspot.com/-QnPOHKoDe9k/UdWkrtlkudI/AAAAAAAAAao/NoTiFsvD1bA/s660/Trading+Pile.jpg" alt="Cards" >
 
    <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/-4iFy6AvJnw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
+     <div id = "frm">
+        <h1>Login</h1>
+        <form name="f1" action = "login.php" onsubmit = "return validation()" method = "POST">  
+            <p>  
+                <label> Email: </label>  
+                <input type = "text" id ="email" name  = "email" />  
+            </p>  
+            <p>  
+                <label> Password: </label>  
+                <input type = "password" id ="password" name  = "password" />  
+            </p>  
+            <p>     
+                <input type =  "submit" id = "btn" value = "Login" />  
+            </p>  
+        </form>  
+    </div>
+    <script>  
+            function validation()  
+            {  
+                var id=document.f1.email.value;  
+                var ps=document.f1.password.value;  
+                if(id.length=="" && ps.length=="") {  
+                    alert("Email and Password fields are empty");  
+                    return false;  
+                }  
+                else  
+                {  
+                    if(id.length=="") {  
+                        alert("Email is empty");  
+                        return false;  
+                    }   
+                    if (ps.length=="") {  
+                    alert("Password field is empty");  
+                    return false;  
+                    }  
+                }                             
+            }  
+        </script>
     
     <form method="POST" action="SignUp.php">
         <button name="subject" type="submit">Sign Up Here</button>
